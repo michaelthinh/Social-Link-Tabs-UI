39bab26c59417605c21550e08a8362ac3bafaa48
20120196 <20120196@student.hcmus.edu.vn>
2023-03-26 16:37:54 +0700

    add javascript to click on each section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("test");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+var tabTitles = $$(".tabs-title-item");
+var tabContents = $$(".tabs-content-item");
+
+var activeTitle = $(".tabs-title-item.active");
+var line = $(".tabs-title .line");
+
+requestIdleCallback(function () {
+    line.style.left = activeTitle.offsetLeft + "px";
+    line.style.width = activeTitle.offsetWidth + "px";
+});
+
+tabTitles.forEach(function (tab, index) {
+    const titleContent = tabContents[index];
+    tab.onclick = function () {
+        $(".tabs-title-item.active").classList.remove("active");
+        $(".tabs-content-item.active").classList.remove("active");
+        line.style.left = this.offsetLeft + "px";
+        line.style.width = this.offsetWidth + "px";
+        this.classList.add("active");
+        titleContent.classList.add("active");
+    };
+});
